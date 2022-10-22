27b977e243e36d7ce340e726c03b948f0e37f066
Edwin Ryerson <eryerson06@gmail.com>
2022-10-21 21:00:47 -0600

    Create Plane3D Constructor

---
+++ Plane3D.java
@@ -0,0 +1,15 @@
+public class Plane3D {
+
+    private double x;
+    private double y;
+    private double z;
+    private double d;
+
+    public Plane3D(double x, double y, double z, double d) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.d = d;
+    }
+    
+}
