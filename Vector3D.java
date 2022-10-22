a1024841daddbb1419f50aab97e04ce7388f8289
Edwin Ryerson <eryerson06@gmail.com>
2022-10-21 18:22:57 -0600

    Create Vector3D Constructor

---
+++ Vector3D.java
@@ -0,0 +1,16 @@
+public class Vector3D {
+
+    private int x;
+    private int y;
+    private int z;
+    
+    public Vector3D(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    
+
+
+}
