c0f56b9a4b43c8d3fcba80019538f62bc7c762e7
Edwin Ryerson <eryerson06@gmail.com>
2022-10-22 17:11:32 -0600

    Add Cross Product Tests, Not Commutative

--- TestAssertionsVector3D.java
+++ TestAssertionsVector3D.java
@@ -29,5 +29,16 @@ public class TestAssertionsVector3D {
         final double dotProduct = vector1.dotProduct(vector2);
         assertTrue(dotProduct == 20.0);
 
+        final Vector3D crossProduct1 = vector1.crossProduct(vector2);
+        final Vector3D crossProduct2 = vector2.crossProduct(vector1);
+        Double[] resultCross1 = {crossProduct1.getX(), crossProduct1.getY(), crossProduct1.getZ()};
+        Double[] expectedCross1 = {16.0, -14.0, 4.0};
+        Double[] resultCross2 = {crossProduct2.getX(), crossProduct2.getY(), crossProduct2.getZ()};
+        Double[] expectedCross2 = {-16.0, 14.0, -4.0};
+        assertArrayEquals(expectedCross1, resultCross1);
+        assertArrayEquals(expectedCross2, resultCross2);
+        // Cross Product is not commutative
+        assertFalse(resultCross1 == resultCross2);
+
     }
 }
