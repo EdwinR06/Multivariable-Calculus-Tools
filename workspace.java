8035ea4344c0d56161406f107c81d169de11e532
Edwin Ryerson <eryerson06@gmail.com>
2022-10-21 22:00:34 -0600

    Add First Display of Vector3D Methods

--- workspace.java
+++ workspace.java
@@ -1,5 +1,14 @@
 public class workspace {
-    private Vector3D vector = new Vector3D(5, 6, 1);
-    private Vector3D vector2 = new Vector3D(1, 2, 3);
+    public static void main(String args[])  
+    {  
+        final Vector3D vector1 = new Vector3D(5, 6, 1);
+        final Vector3D vector2 = new Vector3D(1, 2, 3);
 
+        final Vector3D addition = vector1.add(vector2);
+
+        final String s = String.format("x: %f, y: %f, z: %f", addition.getX(), addition.getY(), addition.getZ());
+
+        System.out.println(s.toString());  //Displays result  
+    }  
+    
 }
