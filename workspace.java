385dcf03b5e0d19409bb433db96a41d4734d9e2b
Edwin Ryerson <eryerson06@gmail.com>
2022-10-22 17:28:35 -0600

    User Input for Workspace Vector1, Vector2 Instance

--- workspace.java
+++ workspace.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 public class Workspace {
-    public static void main(String args[])  
-    {  
-        final Vector3D vector1 = new Vector3D(5, 6, 1);
-        final Vector3D vector2 = new Vector3D(1, 2, 3);
+    public static void main(String args[])  {  
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Vector 1 X: ");
+        double x1 = scanner.nextDouble();
+        System.out.println("Enter Vector 1 Y: ");
+        double y1 = scanner.nextDouble();
+        System.out.println("Enter Vector 1 Z: ");
+        double z1 = scanner.nextDouble();
+
+        System.out.println("Enter Vector 2 X: ");
+        double x2 = scanner.nextDouble();
+        System.out.println("Enter Vector 2 Y: ");
+        double y2 = scanner.nextDouble();
+        System.out.println("Enter Vector 2 Z: ");
+        double z2 = scanner.nextDouble();
+
+
+        final Vector3D vector1 = new Vector3D(x1, y1, z1);
+        final Vector3D vector2 = new Vector3D(x2, y2, z2);
 
         final Vector3D addition = vector1.add(vector2);
 
