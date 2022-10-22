1623fe0224231729cde048d6bbe54d82472776be
Edwin Ryerson <eryerson06@gmail.com>
2022-10-21 19:42:13 -0600

    Rename Add, Subtract, Multiply

--- Vector3D.java
+++ Vector3D.java
@@ -10,17 +10,17 @@ public class Vector3D {
         this.z = z;
     }
 
-    public Vector3D add3D(Vector3D a, Vector3D b) {
+    public Vector3D add(Vector3D a, Vector3D b) {
         final Vector3D vector = new Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
         return vector;
     }
 
-    public Vector3D subtract3D(Vector3D a, Vector3D b) {
+    public Vector3D subtract(Vector3D a, Vector3D b) {
         final Vector3D vector = new Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
         return vector;
     }
 
-    public Vector3D multiply3DByScalar(Vector3D a, int scalar) {
+    public Vector3D multiplyByScalar(Vector3D a, int scalar) {
         final Vector3D vector = new Vector3D(a.x * scalar, a.y * scalar, a.z * scalar);
         return vector;
     }
