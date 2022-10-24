0769876e9c332955eaeb1a5583ddf6f614c76179
Edwin Ryerson <eryerson06@gmail.com>
2022-10-23 20:09:46 -0600

    Add Point3D Class

---
+++ Point3D.java
@@ -0,0 +1,12 @@
+public class Point3D {
+    private double x;
+    private double y;
+    private double z;
+
+    public Point3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+}
