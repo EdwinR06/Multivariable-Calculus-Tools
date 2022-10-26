56163a4c247fbb00dd7274ca0b44e3138fbbf02e
Edwin Ryerson <eryerson06@gmail.com>
2022-10-26 14:07:02 -0600

    Add Sine Class

---
+++ Sin.java
@@ -0,0 +1,15 @@
+public class Sin {
+    private double coefficient;
+    private double period;
+    private double exponent;
+
+    public Sin(double coefficient, double period, double exponent){
+        this.coefficient = coefficient;
+        this.period = period;
+        this.exponent = exponent;
+    }
+
+    
+
+    
+}
