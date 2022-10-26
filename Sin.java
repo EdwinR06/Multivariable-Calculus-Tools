cd9fe3047ef75f91e9a467e8d732539c3a66fa3e
Edwin Ryerson <eryerson06@gmail.com>
2022-10-26 14:07:57 -0600

    Add Sine Derivative Calculator

--- Sin.java
+++ Sin.java
@@ -9,14 +9,20 @@ public class Sin {
         this.exponent = exponent;
     }
 
-    
-
     public double calculateSin(double x) {
         final double sin = this.coefficient * Math.pow(Math.sin(this.period * x), this.exponent);
         return sin;
     }
 
-    
+    public Cos sinDerivative(){
+        final Cos cos;
+        if(this.exponent > 1) {
+            cos = new Cos(this.coefficient * this.exponent * this.period, this.period, this.exponent - 1); 
+        } else {
+            cos = new Cos(this.coefficient * this.period, this.period, this.exponent); 
+        }
+        return cos;
+    }
 
-    
+
 }
