8df4c5bf5efeea61bbfb55bcd25362003fefbc3c
Edwin Ryerson <eryerson06@gmail.com>
2022-10-26 14:04:51 -0600

    Add Cosine Class

---
+++ Cos.java
@@ -0,0 +1,13 @@
+public class Cos {
+    private double coefficient;
+    private double period;
+    private double exponent;
+
+    public Cos(double coefficient, double period, double exponent){
+        this.coefficient = coefficient;
+        this.period = period;
+        this.exponent = exponent;
+    }
+
+    
+}
