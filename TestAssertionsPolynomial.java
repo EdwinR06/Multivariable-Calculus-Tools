505d285c88858cdb9f64a2bededc309328607e51
Edwin Ryerson <eryerson06@gmail.com>
2022-10-27 17:55:52 -0600

    Add Calculate Polynomial at X Test

--- TestAssertionsPolynomial.java
+++ TestAssertionsPolynomial.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.*;
 public class TestAssertionsPolynomial {
     @Test
     public void testAssertionsPolynomial() {
-        
+        Polynomial polynomial = new Polynomial(1, 2);
+
+        double polynomialValue = polynomial.calculatePoly(2);
+        double expectedPolynomial = 4;
+        assertEquals(expectedPolynomial, polynomialValue, 0.01);
     }
 }
