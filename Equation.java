e13a97dbefb2bbb4b8699aae34c496a889fd372a
Edwin Ryerson <eryerson06@gmail.com>
2022-10-26 22:38:08 -0600

    Add Equation at Point Calculator Method

--- Equation.java
+++ Equation.java
@@ -11,7 +11,21 @@ public class Equation {
         this.numTerms = this.listTerms.size();
     }
 
+    public double equationAtPoint(double x) {
+        double result = 0;
+
+
+        for (int i = 0; i < this.numTerms; i++) {
+            if(this.listTerms.get(i).get(0) == 1){
+                Sin sin = new Sin(this.listTerms.get(i).get(1), this.listTerms.get(i).get(2), this.listTerms.get(i).get(3));
+                result += sin.calculateSin(x);
+            } else if (this.listTerms.get(i).get(0) == 2){
+                Cos cos = new Cos(this.listTerms.get(i).get(1), this.listTerms.get(i).get(2), this.listTerms.get(i).get(3));
+                result += cos.calculateCos(x);
+            }
+        }
+            
     
-
-
+        return result;
+    }
 }
