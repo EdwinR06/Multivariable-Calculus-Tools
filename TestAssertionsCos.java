ff93f390b9052b6841de82dc0860564c01500f47
Edwin Ryerson <eryerson06@gmail.com>
2022-10-27 16:58:10 -0600

    Add Calculate Cos at X Test

--- TestAssertionsCos.java
+++ TestAssertionsCos.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.*;
 public class TestAssertionsCos {
     @Test
     public void testAssertionsCos () {
-        
+        Cos cos = new Cos(1, 1, 1);
+
+        double resultCalculate = cos.calculateCos(Math.PI / 2);
+        double expectedCalculate = 0;
+        assertEquals(expectedCalculate, resultCalculate, 0.01);
     }
 }
