fed4d8e2227d1c09771e9adf26ad6c74f8753ce6
Edwin Ryerson <eryerson06@gmail.com>
2022-10-26 22:37:41 -0600

    Add Equation Class Constructor

---
+++ Equation.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.util.ListIterator;
+
+public class Equation {
+    private int numTerms;
+    private ArrayList<ArrayList<Integer>> listTerms;
+
+    public Equation(ArrayList<ArrayList <Integer>> list) {
+        this.listTerms = list;
+        this.numTerms = this.listTerms.size();
+    }
+
+    
+
+
+}
