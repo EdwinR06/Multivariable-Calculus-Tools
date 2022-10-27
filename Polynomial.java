d830d129794b436c7b4f1792c19bba9af818338f
Edwin Ryerson <eryerson06@gmail.com>
2022-10-27 12:13:09 -0600

    Add Polynomial Class

---
+++ Polynomial.java
@@ -0,0 +1,9 @@
+public class Polynomial {
+    private double coefficient;
+    private double exponent;
+    
+    public Polynomial(double coefficient, double exponent){
+        this.coefficient = coefficient;
+        this.exponent = exponent;
+    }
+}
