ebf2d1efaef11ab0afcdb4474e68a0a0cc0e8c41
Edwin Ryerson <eryerson06@gmail.com>
2022-10-27 15:46:53 -0600

    Add Constant Derivative Method

--- Constant.java
+++ Constant.java
@@ -1,7 +1,11 @@
 public class Constant {
     private double coefficient;
-    
+
     public Constant(double coefficient) {
         this.coefficient = coefficient;
+    }
+
+    public double constantDerivative() {
+        return 0;
     }
 }
