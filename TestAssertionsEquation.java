25d941d7e4a3d9ead1190c9ac53a77edfc3eb5ea
Edwin Ryerson <eryerson06@gmail.com>
2022-10-27 18:13:40 -0600

    Add Equation at X Test

--- TestAssertionsEquation.java
+++ TestAssertionsEquation.java
@@ -1,9 +1,28 @@
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TestAssertionsEquation {
     @Test
     public void testAssertionsEquation(){
+        ArrayList<ArrayList<Integer>> equation = new ArrayList<ArrayList <Integer>>();
+        ArrayList<Integer> term1 = new ArrayList<Integer>(Arrays.asList(0, 1, 2));
+        ArrayList<Integer> term2 = new ArrayList<Integer>(Arrays.asList(0, 1, 1)); 
+        ArrayList<Integer> term3 = new ArrayList<Integer>(Arrays.asList(3, 4));  
+        equation.add(term1);
+        equation.add(term2);
+        equation.add(term3);
+        // x^2 + x + 4 = y
+        Equation equation1 = new Equation(equation);
+        
+
+        double equationResult = equation1.equationAtPoint(2);
+        double expectedEquation = 10;
+        assertEquals(expectedEquation, equationResult, 0.01);
+
         
     }
 }
