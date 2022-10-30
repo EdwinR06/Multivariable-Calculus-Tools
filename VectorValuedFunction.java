fc2b8c433ce78ff51cd8a86dcd979e14634e8dca
Edwin Ryerson <eryerson06@gmail.com>
2022-10-29 20:14:46 -0600

    Add Position Method to Vector Valued Function

--- VectorValuedFunction.java
+++ VectorValuedFunction.java
@@ -21,6 +21,15 @@ public class VectorValuedFunction {
         return this.z;
     }
 
+    public Vector3D position(double time) {
+        final double xPos = this.x.equationAtPoint(time);
+        final double yPos = this.y.equationAtPoint(time);
+        final double zPos = this.z.equationAtPoint(time);
+
+        final Vector3D positionVector = new Vector3D(xPos, yPos, zPos);
+        return positionVector;
+    }
+
 
     
 }
