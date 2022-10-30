75c46ad157bfd5d6ef7f55a7f849d5b9f2872c5c
Edwin Ryerson <eryerson06@gmail.com>
2022-10-29 20:02:38 -0600

    Create Vector Valued Functions File, Constructor

---
+++ VectorValuedFunction.java
@@ -0,0 +1,14 @@
+public class VectorValuedFunction {
+    private Equation x;
+    private Equation y;
+    private Equation z;
+
+    public VectorValuedFunction(Equation x, Equation y, Equation z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+    
+}
